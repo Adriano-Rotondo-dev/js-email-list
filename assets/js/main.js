2be75c5a5f85b15edc2c4e0bf8471fc1bb0a6e05
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-16 17:51:21 +0200

    refactor: restructure HTML and JavaScript for email generation functionality, add button evenListener to generate mails

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
+//*salvo l'endpoint in una variabile
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-for (let i = 0; i < 10; i++) {
-  fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+//*salvo i DOM nodes necessari
+const buttonEl = document.getElementById('btn')
+const colEl = document.getElementById('mails')
+
+
+
+//*inizializzo l'eventListener del bottone per generare le e-mail
+
+buttonEl.addEventListener("click", () => {
+    //*inizializzo il ciclo for per generare 10 email 
+    for (let i = 0; i < 10; i++) {
+      fetch(endpoint)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+})
