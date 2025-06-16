bacbf1b5fdcb6839efd33489804d3e5a92d40ccb
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-16 17:15:48 +0200

    implement fetch loop to retrieve and log 10 random email addresses from API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-fetch(endpoint)
-.then (response => response.json())
-.then(data => {
-    console.log(data)
-})
+for (let i = 0; i < 10; i++) {
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
